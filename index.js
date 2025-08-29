6d8aa2f1e87fc88f10b98f34c0440281e108ec5e
ahmed <rawako12@gmail.com>
2025-08-30 00:26:50 +0300

    api fix-2

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const Product = mongoose.model("Product", productSchema);
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://furniro-backend-production-1e15.up.railway.app", // غيره بعدين للفرونت على Railway
+    origin: "https://furniro-backend-production-1e15.up.railway.app/products", // غيره بعدين للفرونت على Railway
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
